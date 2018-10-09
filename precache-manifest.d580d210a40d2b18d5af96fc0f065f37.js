b13e90d57e2eb91ba28e02e3686ec835f17d2e3a
Citlalli DMG <citlallidmg@gmail.com>
2018-10-09 12:44:45 -0500

    Updates

---
+++ precache-manifest.d580d210a40d2b18d5af96fc0f065f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9a7656dcf112c272870",
+    "url": "/static/js/main.b9a7656d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "b9a7656dcf112c272870",
+    "url": "/static/css/main.c9d7b150.chunk.css"
+  },
+  {
+    "revision": "3cc3d4a95c3e649ff5171831bbf2547e",
+    "url": "/index.html"
+  }
+];
